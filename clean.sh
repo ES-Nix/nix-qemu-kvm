e194019fe2cb510dec6cc74fa562a4ae7c1131dd
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-03-21 13:35:45 -0300

    Removes script moved to nix result

--- clean.sh
+++
@@ -1,8 +0,0 @@
-rm --force --recursive --verbose \
-teste.qcow2 \
-ubuntu-20.04-minimal-cloudimg-amd64.img \
-user-data.img \
-disk.qcow2 \
-teste18.04.qcow2 \
-ubuntu-18.04-server-cloudimg-amd64.img
-
--- refresh.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-set -euxo pipefail
-
-
-rm --force --verbose disk.qcow2 userdata.qcow2
