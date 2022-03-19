44478ee6a83c75209d64334683729ce575754415
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2022-03-19 00:06:08 -0300

    Saving it

---
+++ src/utils/runVM-with-volume.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation rec {
+          name = "runVM";
+          buildInputs = with pkgs; [ stdenv ];
+          nativeBuildInputs = with pkgs; [ makeWrapper ];
+          propagatedNativeBuildInputs = with pkgs; [
+            bash
+            coreutils
+            qemu
+          ]
+          ;
+
+          src = builtins.path { path = ./.; name = "runVM"; };
+          phases = [ "installPhase" ];
+
+          unpackPhase = ":";
+
+          installPhase = ''
+            mkdir -p $out/bin
+
+            cp -r "${src}"/* $out
+
+            install \
+            -m0755 \
+            $out/runVM.sh \
+            -D \
+            $out/bin/runVM
+
+            patchShebangs $out/bin/runVM
+
+            wrapProgram $out/bin/runVM \
+              --prefix PATH : "${pkgs.lib.makeBinPath propagatedNativeBuildInputs }"
+          '';
+
+        }
